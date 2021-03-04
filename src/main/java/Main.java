a2346ae7f809609cd1c2e1eb75bdcd999a63748a
Anton Mamakin <antonmamakinscript@gmail.com>
2021-03-04 11:24:43 +0400

    end

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        FileManager fileManager = new FileManager();
+        StringBuilder blackListStr = fileManager.readFile("data/black-list.txt");
+        String[] strings = blackListStr.toString().split("\n");
+        List<String> blackList = Arrays.asList(strings);
+        Moderator moderator = new Moderator(fileManager, blackList, "/input","/output");
+        moderator.CrawlingAllFiles();
     }
 }
